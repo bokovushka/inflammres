f5bf836e8992eab631af9c465e04e826eef8dcb4
bokovushka <57456193+bokovushka@users.noreply.github.com>
2022-11-28 11:31:45 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -5,19 +5,47 @@ import $ from 'jquery';
 
 // functions.isWebp();
 
-//mobile menu
-$(document).ready(function () {
-	let body_lock = document.querySelector('body');
-	let menuBtn = document.querySelector('.navbar-toggler');
-
-	menuBtn.addEventListener('click', function () {
-		body_lock.classList.toggle('lock');
-	})
-});
-
 //cookies modal
 $(document).ready(function () {
 	setTimeout(function () {
 		$('#popup-cookies').click();
 	}, 1000);
 });
+
+//animation
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+}
